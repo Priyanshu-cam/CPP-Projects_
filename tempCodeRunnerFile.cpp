615b4310696b73c79b71fd257ea85d4140d7fcd0
Priyanshu-cam <priyanshuj103@gmail.com>
2026-07-21 23:05:53 +0530

    create space invader game and login/signup system

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,51 +1,78 @@
-// Rock-paper scissor game 
-#include<iostream>
-#include<cstdlib>
-#include<ctime>
+#include<cstdio>
+#include<GL/glew.h> //OpenGL is implemented by hardware drivers (NVIDIA, AMD, Intel). Because of this, new OpenGL functions aren't automatically available in C++ at compile-time.
+#include<GLFW/glfw3.h> //OpenGL does not know how to create a desktop window, track a mouse, or detect a keyboard press (like pressing Space to shoot). GLFW bridges that gap. It handles cross-platform window management and OS input events.
 
-using namespace std; 
+#define GL_ERROR_CASE(glerror)\
+case glerror: snprintf(error, sizeof(error), "%s", #glerror)
 
-int main(){
-    cout<<"==============================" <<endl;
-    cout<<"Welcome to Rock-Paper-Scissors Game! "<<endl;
-    cout<<"==============================" <<endl;
-    cout<<"Let's play the Game!" <<endl;
+inline void gl_debug(const char *file,  int line){
+    GLenum err; 
+    while((err = glGetError()) != GL_NO_ERROR){
+        char error[128]; 
 
-    int playerScore = 0; 
-    int computerScore = 0; 
-
-    int playerChoice; 
-    cout<<"Enter your choice: 1.Rock, 2.Paper, 3.Scissors: "<<endl;
-    cin>>playerChoice; 
-
-    int computerChoice = rand() % 3 + 1; // this will generate a random choice from the computer 
-
-    if(computerChoice == 1)cout<<"Computer chose Rock"<<endl;
-    else if(computerChoice == 2) cout<<"Computer chose Paper"<<endl;
-    else cout<<"Computer chose Scissors"<<endl;
-
-    if(playerChoice == computerChoice){ //Tie condition
-        cout<<"It's a tie!"<<endl;
+        switch(err){
+            GL_ERROR_CASE(GL_INVALID_ENUM); break; 
+            GL_ERROR_CASE(GL_INVALID_VALUE); break;
+            GL_ERROR_CASE(GL_INVALID_OPERATION); break;
+            GL_ERROR_CASE(GL_INVALID_FRAMEBUFFER_OPERATION); break;
+            GL_ERROR_CASE(GL_OUT_OF_MEMORY); break;
+            default: snprintf(error, sizeof(error), "%s", "UNKNOWN_ERROR"); break;
+        }
+        fprintf(stderr, "%s - %s: %d\n", error, file, line); 
     }
-    else if(playerChoice == 1 && computerChoice == 3){ // Rock vs Scissor -> Rock wins
-        cout<<"You win!"<<endl;
-        playerScore++;
+}
+
+#undef GL_ERROR_CASE
+
+void error_Callback(int error, const char* description){
+    fprintf(stderr, "Error: %s\n", description); 
+}
+
+int main(int argc, char* argv[]){
+    glfwSetErrorCallback(error_Callback); 
+    GLFWwindow* window; 
+    if(!glfwInit()) return -1; 
+
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE); 
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3); 
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3); 
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); 
+
+    /*Create a windowed mode window and its OpenGL context*/
+    window = glfwCreateWindow(640, 480, "Space Invaders", NULL, nullptr); 
+    if(!window){
+        glfwTerminate(); 
+        return -1; 
     }
-    else if(playerChoice == 2 && computerChoice == 1){ // Paper vs Rock -> Paper wins
-        cout<<"You Win!"<<endl;
-        playerScore++;
-    }else{ // Scissors vs Paper -> Scissors wins
-        cout<<"Computer Wins!"<<endl;
-        computerScore++;
+
+    glfwMakeContextcurrent(window); 
+
+    Glenum err = glewInit();
+    if(err != GlEW_OK){
+        fprintf(stderr, "Error: %s\n", glewGetErrorString(err)); 
+        glfwTerminate(); 
+        return -1; 
     }
-    if(playerScore > computerScore){
-        cout<<"Congratulations! You won the game!"<<endl;
-        cout<<"You have defeated the computer with a score of "<<playerScore - computerScore<<endl;
+
+    int glVersion[2] = {-1, 1}; 
+    glGetIntegerv(GL_MAJOR_VERSION, &glVersion[0]); 
+    glGetIntegerv(GL_MINOR_VERSION, &glVersion[1]); 
+
+    gl_debug(__FILE__, __LINE__); 
+    printf("Using OpenGL %d.%d\n", glVersion[0], glVersion[1]); 
+    printf("Renderer used: %s\n", glGetString(GL_RENDERER)); 
+    printf("Shading Language: %s\n", glGetString(GL_SHADING_LANGUAGE_VERSION));
+
+    glClearColor(1.0, 0.0, 0.0, 1.0); 
+    while(!glfwWindowShouldClose(window)){
+
+        glClear(GL_COLOR_BUFFER_BIT); 
+        glfwSwapBuffers(window);
+        glfwPollEvents(); 
     }
-    else{
-        cout<<"Computer won the game!"<<endl;
-        cout<<"Better luck next time! Computer has defeated you with a score of "<<computerScore - playerScore<<endl;
-    }
+    
+    glfwDestroyWindow(window); 
+    glfwTerminate();
 
     return 0; 
 }
